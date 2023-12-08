862e3d6cc77a81a586ee96f1a9b63c9cefa88bda
samuel <buzonsamuel18@gmail.com>
2023-12-07 20:56:00 -0500

    update

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon/';
-const searchInput = document.getElementById('search');
-const pokedexContainer = document.getElementById('pokedex');
+//tomar la api en una variavle
+const API = 'https://pokeapi.co/api/v2/pokemon/';
+//obtener los elementos del html
+const buscar = document.getElementById('search');
+const pokedex = document.getElementById('pokedex');
 
 // Función para mostrar un mensaje de error
 function showError(message) {
-    pokedexContainer.innerHTML = `<p class="error">${message}</p>`;
+    pokedex.innerHTML = `<p class="error">${message}</p>`;
 }
 
 // Función para buscar un Pokémon
 async function searchPokemon() {
     // Obtener el valor del campo de búsqueda y convertirlo a minúsculas
-    const searchedPokemon = searchInput.value.toLowerCase();
+    const BuscarPokemon = buscar.value.toLowerCase();
 
     try {
         // Realizar una petición a la API de PokeAPI con el nombre del Pokémon
-        const response = await fetch(URL + searchedPokemon);
+        const response = await fetch(API + BuscarPokemon);
         if (!response.ok) {
             // Si la respuesta no es exitosa, mostrar un mensaje de error
-            showError(`No se encontró ningún Pokémon llamado "${searchedPokemon}"`);
+            showError(`No se encontró ningún Pokémon llamado "${BuscarPokemon}"`);
             return;
         }
 
@@ -25,13 +27,15 @@ async function searchPokemon() {
         const data = await response.json();
 
         // Mostrar los datos del Pokémon en el contenedor de resultados
-        pokedexContainer.innerHTML = 
+        pokedex.innerHTML = 
         `
+            <div class="card-container">
             <h2>${data.name.toUpperCase()}</h2>
             <img src="${data.sprites.front_default}" alt="${data.name}">
             <p>Número: ${data.id}</p>
             <p>Altura: ${data.height / 10}m</p>
             <p>Peso: ${data.weight / 10}km</p>
+            </div>
         `;
     } catch (error) {
         // Si ocurre algún error durante la petición, mostrar un mensaje de error
